7db806931bd24e05f25a2063e5be8643dfff7dbc
ComputingDevice <jaimegelopez@gmail.com>
2020-09-10 10:08:51 -0400

    updated for eLife revision

--- Code/ode_dilution_code/run_batch_scripts.m
+++ Code/ode_dilution_code/run_batch_scripts.m
@@ -8,16 +8,21 @@ m = 3;
 p = 2;
 s = transpose(linspace(0,1,m));
 s = [s , 1-s];
-P = [1;0];
+P = [0.3;0.7];
 K = 1;
 bo = 1/m;
 chi = [0 0; 1 0];
+variable_rc = 1e-1;
+variable_K = [1;2];
+variable_Y = [2;1];
 plt = 1;
 
 %Simulate
 tic
 [b,Nr,Bstore,Cstore,tstore] = multispeciesbatchMADAPT(bo,Bt,Ct,s,P,m,p,plt,K);
 %[b,Nr,Bstore,Cstore,tstore] = multispeciesbatchMADAPT_troph(bo,Bt,Ct,s,P,m,p,plt,K,chi);
+%[b,Nr,Bstore,Cstore,tstore] = multispeciesbatch_variable_end(bo,Bt,Ct,s,P,m,p,plt,K,variable_rc);
+%[b,Nr,Bstore,Cstore,tstore] = multispeciesbatchMADAPT_variable_K_Y(bo,Bt,Ct,s,P,m,p,plt,variable_K,variable_Y);
 toc
 
 %Compute growth metrics
--- Code/ode_dilution_code/run_dilution_scripts.m
+++ Code/ode_dilution_code/run_dilution_scripts.m
@@ -1,5 +1,6 @@
 clear;clc
-%Test script for dilution codes
+%Test script for dilution codes. For every variant of the model, we created
+%a separate set of scripts.
 
 Bt = 1000;
 Ct = 1000;
@@ -15,6 +16,10 @@ spike = 0.05;
 batch_limit = 100; 
 P_cycle = [0 1; 1 0];
 errtype = 1;
+variable_K = [1000;2000];
+variable_Y = [2;1];
+variable_rc = 1e-1;
+max_batch = 1000;
 plt = 1;
 
 %Simulate
@@ -23,9 +28,11 @@ tic
 %[b,Nr] = serialdilMADAPT_spike(Bt,Ct,s,P,m,p,plt,bo,K,spike,batch_limit);
 %[b,Nr] = serialdilMADAPT_troph(Bt,Ct,s,P,m,p,plt,bo,errtype,K,chi);
 %[b,Nr] = serialdilMADAPT_bolus_cycling(Bt,Ct,s,P_cycle,m,p,plt,bo,K);
+%[b,Nr] = serialdilMADAPT_variable_K_Y(Bt,Ct,s,P,m,p,plt,bo,errtype,variable_K,variable_Y);
+%[b,Nr] = serialdil_variable_end(Bt,Ct,s,P,m,p,plt,bo,errtype,K,variable_rc,max_batch);
 toc
 
-%Verification
+%Verification 
 growth = s*transpose(Nr); %Compute the fitnesses
 mult = exp(growth); %Verifying that the growth factors are all the same
 g = log((Ct+Bt)/Bt);  %Theoretical fitness prediction
